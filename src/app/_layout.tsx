a5a3ca33f7dc5747b04df659c721c8481a17d271
GuilhermeFT <guilhermetrind12@gmail.com>
2025-09-24 11:17:54 -0300

    fix: change libs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import Colors from '../constants/Colors'
-import * as SplashScreen from 'expo-splash-screen'
 
 import { useEffect, useState } from 'react'
 
